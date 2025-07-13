52d43f7376432a1f640ed82807861404d0e6bdb6
kobaoo <koblandikz@gmail.com>
2025-07-13 17:17:37 +0500

    add: migrations

---
+++ internal/adapters/db/migrations/init.sql
@@ -0,0 +1,27 @@
+CREATE TABLE user_sessions {
+    id SERIAL PRIMARY KEY,
+    session_token TEXT UNIQUE NOT NULL,
+    name TEXT NOT NULL,
+    avatar_url TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW() 
+};
+
+CREATE TABLE posts {
+    id SERIAL PRIMARY KEY,
+    session_id INTEGER REFERENCES user_sessions(id) ON DELETE CASCADE,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL, 
+    image_url TEXT,
+    created_at TIMESTAMP DEFAULT NOw(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    is_archieved BOOLEAN DEFAULT FALSE
+};
+
+CREATE TABLE comments {
+    id SERIAL PRIMARY KEY,
+    session_id INTEGER REFERENCES user_sessions(id) ON DELETE CASCADE,
+    post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+    parent_comment_id INTEGER REFERENCES comments(id) ON DELETE CASCADE,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+};
